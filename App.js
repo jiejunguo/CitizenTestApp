eeb82e5eaa39d02b8d5fbe74360fd6b9e3cfe517
Gloria <jiejunguo@gmail.com>
2021-03-12 09:38:48 -0800

    Create Test Screen

--- App.js
+++ App.js
@@ -2,26 +2,8 @@ import * as React from "react";
 import { Button, View, Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate("Details")}
-      />
-    </View>
-  );
-}
-
-function DetailsScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Details Screen</Text>
-    </View>
-  );
-}
+import HomeScreen from "./src/screens/HomeScreen";
+import TestScreen from "./src/screens/TestScreen";
 
 const Stack = createStackNavigator();
 
@@ -30,7 +12,7 @@ function App() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Test" component={TestScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/TestScreen.js
+++ src/screens/TestScreen.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
-function HomeScreen({ navigation }) {
+function TestScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-      <Button title="Press me" onPress={() => navigation.navigate("Test")} />
+      <Text>Test Screen</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({});
 
-export default HomeScreen;
+export default TestScreen;
