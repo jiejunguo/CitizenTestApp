607144f5efabf5b257ea5907f33cb15ec02a81d0
Gloria <jiejunguo@gmail.com>
2021-03-16 09:12:53 -0700

    Navigation between Screens

--- src/screens/Homescreen.js
+++ src/screens/Homescreen.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
+import questions from "../../all_questions.json";
 
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen</Text>
-      <Button title="Press me" onPress={() => navigation.navigate("Test")} />
+      <Text>Australian Citizen Test</Text>
+      <Button title="Start Test" onPress={() => navigation.navigate("Test")} />
     </View>
   );
 }
